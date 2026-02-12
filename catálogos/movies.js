a5996f22f8475ec6476f8b800ec77b16076f1f32
rafasalsa75 <139479350+rafasalsa75@users.noreply.github.com>
2026-02-12 10:54:36 +0100

    Create movies.js

---
+++ catálogos/movies.js
@@ -0,0 +1,10 @@
+[
+  {
+    "id": "tt001",
+    "name": "Ejemplo Película",
+    "poster": "https://tu-servidor.com/poster.jpg",
+    "background": "https://tu-servidor.com/bg.jpg",
+    "genres": ["Acción", "Drama"],
+    "description": "Película de ejemplo en español."
+  }
+]
