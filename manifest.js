c48680d3c012bce1e4bdabe55359382cdd128a09
rafasalsa75 <139479350+rafasalsa75@users.noreply.github.com>
2026-02-12 10:45:54 +0100

    Create manifest.js for LumeStream addon
    Added a manifest file for the LumeStream addon with details about its categories and streaming links.

---
+++ manifest.js
@@ -0,0 +1,13 @@
+{
+  "id": "org.tuusuario.lumestream",
+  "version": "1.0.0",
+  "name": "LumeStream Mejorado",
+  "description": "Addon de Stremio con más categorías y enlaces de streaming",
+  "resources": ["catalog", "stream"],
+  "types": ["movie", "series", "channel", "documentary"],
+  "idPrefixes": ["tt"],
+  "catalogs": [
+    { "type": "movie", "id": "top", "name": "Top Películas" },
+    { "type": "series", "id": "top", "name": "Top Series" }
+  ]
+}
