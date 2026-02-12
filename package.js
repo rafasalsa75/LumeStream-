b458a974b7c00fe8080b3e37df44dabe3c54eb7a
rafasalsa75 <139479350+rafasalsa75@users.noreply.github.com>
2026-02-12 09:13:39 +0100

    Add package.json for lumestream project

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "lumestream",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "stremio-addon-sdk": "^0.6.0",
+    "node-fetch": "^3.3.1"
+  },
+  "type": "module"
+}
